7c91e01c84c541ca06cdb0a724d19cdcdb633203
Jonathan Netley <jnetley@fullfacing.com>
2021-02-24 11:47:55 +0200

    added remove hero's function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react'
-// import HeroList from './components/HeroList'
+import HeroList from './components/HeroList'
 import { heroDB } from './data';
 
 class App extends React.Component {
@@ -8,19 +8,65 @@ class App extends React.Component {
     super(props);
     this.state = {
       data: heroDB,
-      entry: ''
+      entry: "",
     };
   }
 
-  render(){
+  handleEntryInput() {
+    let { data } = this.state;
+    let heroName = document.querySelector("#hname").value;
+    this.setState({
+      data: data,
+      entry: heroName,
+    });
+  }
+
+  generateId() {
+    let random = Math.random();
+    let twoDes = random * 100;
+    return Math.round(twoDes, 0);
+  }
+
+  addHero() {
+    let id = this.generateId();
+    let { data, entry } = this.state;
+    let updatedHeros = data.concat({
+      heroName: entry,
+      id: id,
+    });
+
+    this.setState({
+      data: updatedHeros,
+      entry: null,
+    });
+  }
+
+  removeHero(id) {
+    let {data} = this.state
+    let updated = data.filter(hero => hero.id == id)
+    this.setState({
+      data: updated
+    })
+  }
+
+  render() {
+    let { data } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
-        <div id="app">
-        </div>
+          <div id="app"></div>
         </header>
+
+        <label for="hname">Hero name</label>
+        <input id="hname" type="text"></input>
+        <input type="button" onClick={this.handleEntryInput}>
+          Add Hero
+        </input>
+        <HeroList data={data} handleHeroRemove={this.removeHero} />
       </div>
-    )};
+    );
+  }
 }
 
 export default App;
--- src/components/HeroList.js
+++ src/components/HeroList.js
@@ -1,8 +1,10 @@
+export default HeroList in HeroList.js
+
 class HeroList extends React.Component {
 
     render() {
         const {data, handleHeroRemove} = this.props
-        const heros = data.map(hero => <p>hero</p>)
+        const heros = data.map((hero, id) => <p onClick={this.handleHeroRemove(id)}>{hero}</p>)
 
         return (heros)
     }
